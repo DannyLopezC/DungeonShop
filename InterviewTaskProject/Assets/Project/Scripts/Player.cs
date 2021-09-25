c0a69310dc423c233b00e3c00c2004f335b47d0c
Danny López Cárdenas <70038382+DannyLopezC@users.noreply.github.com>
2021-09-24 21:37:25 -0500

    Player movement

---
+++ InterviewTaskProject/Assets/Project/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D _collider;
+    private Vector3 _moveDelta;
+
+    private void Start()
+    {
+        _collider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _moveDelta = Vector3.zero;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //set delta
+        _moveDelta = new Vector3(x, y);
+
+        //change facing direction
+        if (_moveDelta.x > 0) transform.localScale = Vector3.one;
+        else if (_moveDelta.x < 0) transform.localScale = new Vector3(-1, 1, 1);
+
+        //movement
+        transform.Translate(_moveDelta * Time.deltaTime);
+    }
+}
