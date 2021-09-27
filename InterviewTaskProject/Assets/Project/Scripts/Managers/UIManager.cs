b127748eeba83ab3210f552b1141c7943b7eaf8b
Danny López Cárdenas <70038382+DannyLopezC@users.noreply.github.com>
2021-09-27 01:15:19 -0500

    First interactable ui

--- InterviewTaskProject/Assets/Project/Scripts/Managers/UIManager.cs
+++ InterviewTaskProject/Assets/Project/Scripts/Managers/UIManager.cs
@@ -2,25 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
     public Button inventoryButton;
 
     public GameObject menuGo;
-    private Canvas _menuCanvas;
-    public float menuMotion;
+    public Animator animator;
+    public bool opened = false;
+    public bool changing = false;
 
     private void Start()
     {
-        _menuCanvas = menuGo.GetComponent<Canvas>();
+
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) OnInventory(!menuGo.activeInHierarchy);
-
+        if (Input.GetKeyDown(KeyCode.Escape)) OnInventory(!opened);
     }
 
-    public void OnInventory(bool open) => menuGo.SetActive(open);
+    public void OnInventory(bool open)
+    {
+        if (changing) return;
+
+        Image buttonImage = inventoryButton.gameObject.GetComponent<Image>();
+
+        if (open)
+        {
+            changing = true;
+            buttonImage.DOFade(0, 0.2f)
+                .OnComplete(() =>
+                {
+                    inventoryButton.gameObject.SetActive(!open);
+                    changing = false;
+                });
+
+            animator.SetTrigger("show");
+            opened = true;
+        }
+        else
+        {
+            changing = true;
+            buttonImage.DOFade(100, 0.2f)
+                .OnComplete(() =>
+                {
+                    inventoryButton.gameObject.SetActive(!open);
+                    changing = false;
+                });
+
+            animator.SetTrigger("hide");
+            opened = false;
+        }
+
+    }
 }
